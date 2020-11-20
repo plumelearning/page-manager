62c883665512f673730ae69551a87a3b6c69cb63
John Farrell MacDonald <john@jfmacdonald.com>
2020-11-20 13:29:55 -0800

    update copyright notice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
 @license
-Copyright 2019, 2020 Strategic Technology Solutions DBA Plum eLearning
+Copyright 2018, 2019, 2020 Strategic Technology Solutions DBA Plum eLearning
 
 
 Licensed under the Apache License, Version 2.0 (the "License");
